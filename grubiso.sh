9ef820502774a596f0b8902343e6caa7c095fc2e
Wunst <matthiesbe@gmail.com>
2023-10-03 16:12:30 +0200

    Prepare a RAM disk for testing
    RAM disk is loaded as a Multiboot 2 module and page-aligned
    Now we need to get its address from the info structure and map it in
    memory

--- grubiso.sh
+++ grubiso.sh
@@ -6,5 +6,6 @@ make install-all
 fi
 mkdir -p sysroot/boot/grub
 cp grub.cfg sysroot/boot/grub/grub.cfg
+cp ramdisk sysroot/boot/ramdisk
 grub-mkrescue sysroot -o asternix.iso
 qemu-system-i386 -cdrom asternix.iso -s
