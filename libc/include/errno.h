d056132acbcc0c754370788ad2f0d8778fd7dadb
Wunst <matthiesbe@gmail.com>
2023-10-10 17:35:06 +0200

    documentation: Document part of libk (errno, stdio)
    Mostly stuff that already had docs, just not doxygen-style

--- libc/include/errno.h
+++ libc/include/errno.h
@@ -1,9 +1,11 @@
-/*
- * file: libc/include/errno.h
+/**
+ * @file libc/include/errno.h
  *
- * POSIX system error numbers.
- * https://pubs.opengroup.org/onlinepubs/9699919799/basedefs/errno.h.html
+ * @brief POSIX system error numbers
+ * 
+ * @see https://pubs.opengroup.org/onlinepubs/9699919799/basedefs/errno.h.html
  */
+
 #ifndef __ERRNO_H
 #define __ERRNO_H
 
@@ -11,30 +13,30 @@
  * implemented, may still be in use somewhere so we don't reassign it. */
 #define _ELEGACY 1
 
-/*
- * Permission denied.
+/**
+ * @brief Permission denied.
  */
 #define EACCES 2
 
-/*
- * File exists.
+/**
+ * @brief File exists.
  */
 #define EEXIST 3
 
-/*
- * File too large.
+/**
+ * @brief File too large.
  * e.g. writing to a position past the maximum allowed offset (file size)
  */
 #define EFBIG 4
 
-/*
- * Operation not permitted.
+/**
+ * @brief Operation not permitted.
  * e.g. creating a symlink or device file on a FAT file system
  */
 #define EPERM 5
 
-/*
- * Bad address.
+/**
+ * @brief Bad address.
  * e.g. a process tries to `read()` into a buffer owned by another process.
  * Because `read()` is a syscall, it runs in kernel mode and this wouldn't be
  * prevented by x86 memory protection, so the kernel needs to detect this sort
@@ -42,23 +44,23 @@
  */
 #define EFAULT 6
 
-/*
- * No space left on device.
+/**
+ * @brief No space left on device.
  */
 #define ENOSPC 7
 
-/*
- * Is a directory.
+/**
+ * @brief Is a directory.
  */
 #define EISDIR 8
 
-/*
- * Not a directory or symbolic link to a directory.
+/**
+ * @brief Not a directory or symbolic link to a directory.
  */
 #define ENOTDIR 9
 
-/*
- * No such device.
+/**
+ * @brief No such device.
  */
 #define ENODEV 10
 
--- libc/include/stdio.h
+++ libc/include/stdio.h
@@ -1,10 +1,71 @@
+/**
+ * @file libc/include/stdio.h
+ * 
+ * @brief C standard input/output
+ * 
+ * @see https://pubs.opengroup.org/onlinepubs/9699919799/basedefs/stdio.h.html
+ */
+
 #ifndef __STDIO_H
 #define __STDIO_H
 
 #ifdef __is_kernel
 
+/**
+ * @brief Print one character
+ * 
+ * @arg ch Character (converted to `char`)
+ * @returns ch
+ * 
+ * @section libk
+ * 
+ * Displays printed character on virtual console.
+ */
 int putchar(int ch);
+
+/**
+ * @brief Print string
+ * 
+ * @arg s Null-terminated string
+ * @returns Number of characters printed
+ * 
+ * @section libk
+ * 
+ * Displays printed string on virtual console.
+ */
 int puts(const char *s);
+
+/**
+ * @brief Print format
+ * 
+ * @arg format Format string
+ * @returns Number of character printed
+ * 
+ * @section libk
+ * 
+ * Displays printed string on virtual console (analogous to the operator
+ * console on the original Unix systems).
+ * 
+ * @par Format specifiers
+ * The following format specifiers are supported:
+ *  Specifier   | Argument type  | Description
+ * -------------|----------------|---------------------------------------------
+ *  `%%`        | --             | Prints the '`%`' character
+ *  `%%c`       | `char`         | Prints a character
+ *  `%%s`       | `const char *` | Prints a string
+ *  `%%d`/`%%i` | `int`          | Converts a signed integer to decimal
+ *  `%%u`       | `unsigned`     | Converts an unsigned integer to decimal
+ *  `%%x`/`%%X` | `unsigned`     | Converts an unsigned integer to hexadecimal
+ *  `%%o`       | `unsigned`     | Converts an unsigned integer to hexadecimal
+ *  `%%n`       | `int *`        | Returns number of characters written so far
+ * 
+ * The following modifiers are supported:
+ * - `0`
+ * - integer value for length
+ * 
+ * @see https://en.cppreference.com/w/cpp/io/c/fprintf
+ * for all format specifiers defined by the standard
+ */
 int printf(const char *format, ...);
 
 #endif
